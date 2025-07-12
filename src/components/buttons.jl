83c0921d69e40042a3e9cdf36569f54bca0eec7f
Erik Buer <buer94@gmail.com>
2025-07-12 11:08:42 +0200

    add IconButton
    Signed-off-by: Erik Buer <buer94@gmail.com>

---
+++ src/components/buttons.jl
@@ -0,0 +1,18 @@
+"""
+A button containing text only.
+"""
+function TextButton(text::String; style=ContainerStyle(), on_click::Function=() -> nothing)
+    return Container(Text(text), style=style, on_click=on_click)
+end
+
+"""
+A button consisting of the icon only.
+"""
+function IconButton(
+    image_path::String;
+    width_px::Union{Nothing,Float32}=nothing,
+    height_px::Union{Nothing,Float32}=nothing,
+    on_click::Function=() -> nothing
+)
+    return Container(Image(image_path; width_px=width_px, height_px=height_px), style=ContainerStyle(), on_click=on_click)
+end
