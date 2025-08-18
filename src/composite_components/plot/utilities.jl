528ec97184a5c4a58d4731dd701574a848f6f696
Erik Buer <buer94@gmail.com>
2025-08-18 10:58:27 +0200

    plot: remove unused functions. Cleanup drawing file
    Signed-off-by: Erik Buer <buer94@gmail.com>

--- src/composite_components/plot/utilities.jl
+++ src/composite_components/plot/utilities.jl
@@ -15,3 +15,22 @@ function get_element_bounds(element::AbstractPlotElement)::Tuple{Float32,Float32
     end
     return (0.0f0, 1.0f0, 0.0f0, 1.0f0)  # Default bounds
 end
+
+"""
+Calculate cumulative distance along a line defined by points.
+"""
+function calculate_line_progress(points::Vector{Point2f})::Vector{Float32}
+    if length(points) < 2
+        return Float32[]
+    end
+
+    progress = Vector{Float32}(undef, length(points))
+    progress[1] = 0.0f0
+
+    for i in 2:length(points)
+        segment_length = norm(points[i] - points[i-1])
+        progress[i] = progress[i-1] + segment_length
+    end
+
+    return progress
+end
