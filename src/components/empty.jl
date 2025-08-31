184c25fe9c6d63333788561c88157f4c997f8f28
Erik Buer <buer94@gmail.com>
2025-08-31 20:51:23 +0200

    remove duplicate "empty.jl" file
    Signed-off-by: Erik Buer <buer94@gmail.com>

--- src/components/empty.jl
+++
@@ -1,9 +0,0 @@
-struct EmptyView <: AbstractView end
-
-function Empty()::EmptyView
-    return EmptyView()
-end
-
-function interpret_view(view::EmptyView, x::Float32, y::Float32, width::Float32, height::Float32, projection_matrix::Mat4{Float32})
-    # Do nothing, as this is an empty view
-end
