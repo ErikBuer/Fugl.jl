9f9d188cf75a1e21d37e0a5201868bdc26b30eea
Erik Buer <buer94@gmail.com>
2025-12-25 17:48:30 +0100

    Empty: implement measure methods

--- src/components/Empty.jl
+++ src/components/Empty.jl
@@ -7,3 +7,15 @@ end
 function interpret_view(view::EmptyView, x::Float32, y::Float32, width::Float32, height::Float32, projection_matrix::Mat4{Float32}, mouse_x::Float32, mouse_y::Float32)
     # Do nothing, as this is an empty view
 end
+
+function measure(view::EmptyView)::Tuple{Float32,Float32}
+    return (0.0f0, 0.0f0)
+end
+
+function measure_width(view::EmptyView, available_height::Float32)::Float32
+    return 0.0f0
+end
+
+function measure_height(view::EmptyView, available_width::Float32)::Float32
+    return 0.0f0
+end
